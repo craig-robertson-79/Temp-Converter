059f1d29db3b976a70a21776b169805c51d1325e
Craig Robertson <17574131+craig-robertson-79@users.noreply.github.com>
2024-03-05 12:04:38 +0000

    Add files via upload

---
+++ components/DisplayTemperature/DisplayTemperature.jsx
@@ -0,0 +1,10 @@
+import {s} from "./DisplayTemperature.style";
+import {Text} from "react-native";
+
+export function DisplayTemperature({temperature, unit}) {
+    return (
+        <Text style={s.temperatureText}>
+            {temperature} {unit}
+        </Text>
+    );
+}
